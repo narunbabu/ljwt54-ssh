473e24da3e9b9ca05b96af52e194f6781a2d6336
Arun Babu N <ab@ameyem.com>
2017-11-16 04:26:42 +0530

    updated state & village

--- app/Http/Controllers/SthaliController.php
+++ app/Http/Controllers/SthaliController.php
@@ -5,36 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use App\District;
+use App\State;
 
 class SthaliController extends Controller
 {
+  public function indexst()
+  {   
+    // $states = DB::table('states')->get();
+    return State::all();  
 
-   public function index()
+  }  
+
+   public function showst( $state_code)
    {
-       return District::all();
+    return $state=State::where('state_code', $state_code)->firstOrFail();
    }
 
-   public function show( $dist_code)
+   public function showd($state_code)
    {
-    //return $request;//  "hello this";
-    //  return District::find($dist_code);
-     $sub_dist=DB::select('select `sub_dist_code`, `sub_dist_name` from `subdistricts` where `subdistricts`.`dist_code` ='.$dist_code);
-     return $sub_dist;
+      //  return District::all();
+      //  return $districts = District::where('state_code', $state_code)->findOrFail();
+       return $districts = DB::table('districts')->where('state_code','=', $state_code)->get(); 
+   }
+
+   public function showsds( $dist_code)
+   {
+    return $subdistricts = DB::table('subdistricts')->where('dist_code','=', $dist_code)->get();    
    }
 
    public function show_vils( $sub_dist_code)
    {
-    //return $request;//  "hello this";
-    //  return District::find($dist_code);
-     $vils=DB::select('select `vil_code`, `vil_name` from `villages` where `villages`.`sub_dist_code` ='.$sub_dist_code);
+    // return $vils = DB::table('village_details')->where('sub_dist_code','=', $sub_dist_code)->get();  
+     $vils=DB::select('select `vil_code`, `vil_name` from `village_details` where `village_details`.`sub_dist_code` ='.$sub_dist_code);
      return $vils;
    }
    public function show_village( $vil_code)
    {
     //return $request;//  "hello this";
     //  return District::find($dist_code);
-     $vils=DB::select('select * from `villages` where `villages`.`vil_code` ='.$vil_code);
-     return $vils;
+     $vil=DB::select('select `vil_name`,`pin_code`,`tot_geograph_area`,`tot_households` from `village_details` where `village_details`.`vil_code` ='.$vil_code.' limit 1');
+     return $vil;
    }
 
 }
---
+++ app/LimitedVillage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LimitedVillage extends Model
+{
+    protected $fillable = ['dist_code', 'dist_name'];
+}
